0a5dd3049f5a6972293053e3af3062ea062ab486
Ryouko <aerivenos@gmail.com>
2016-05-20 17:48:38 +1000

    Added document title.

--- client/modules/core/components/main_layout.jsx
+++ client/modules/core/components/main_layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Grid, Row } from 'react-bootstrap';
+import DocumentTitle from 'react-document-title';
 import Navigation from './navigation';
 
 // TODO: Nav bar
 const Layout = ({ content = () => null }) => (
   <div>
+    <DocumentTitle title="Kancolle Tracker" />
     <Navigation />
     <Grid>
       <Row>
